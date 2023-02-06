4f19d8c86a57c7b52d699e7f595e206f45e116e7
Laurens256 <duinlaurens2@gmail.com>
2023-02-06 13:58:59 +0100

    mmmmmm

--- visitekaartje/scripts/main.ts
+++ visitekaartje/scripts/main.ts
@@ -1,1 +1,12 @@
+const fetchData = async () => {
+	const response: any = await fetch(
+		'https://cors-anywhere.herokuapp.com/https://whois.fdnd.nl/api/v1/member?id=cldczhjad16yh0av08jxscp0a'
+	);
+	console.log(response);
+};
+fetchData();
 
+let albumState: 0 | 1 = 0;
+const flipAlbum = () => {
+
+};

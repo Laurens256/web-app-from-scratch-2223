259e5ee513557cbf0f61ddd704df3d715989639f
Laurens256 <duinlaurens2@gmail.com>
2023-02-06 19:04:44 +0100

    add flipping functionality

--- visitekaartje/scripts/main.ts
+++ visitekaartje/scripts/main.ts
@@ -1,12 +1,38 @@
-const fetchData = async () => {
-	const response: any = await fetch(
-		'https://cors-anywhere.herokuapp.com/https://whois.fdnd.nl/api/v1/member?id=cldczhjad16yh0av08jxscp0a'
+const albumPages: NodeListOf<HTMLElement> = document.querySelectorAll('.album > section');
+
+const initAlbum = () => {
+	for (let i = 0; i < albumPages.length; i++) {
+		albumPages[i].style.zIndex = String(albumPages.length - i);
+	}
+};
+
+const nextButton = document.querySelector('.next-page-btn')!;
+const prevButton = document.querySelector('.prev-page-btn')!;
+
+let currentPage = 0;
+const nextPage = () => {
+	if (currentPage >= albumPages.length - 1) return;
+
+	albumPages[currentPage].classList.add('flipped');
+	console.log(albumPages[currentPage]);
+	albumPages[currentPage].addEventListener(
+		'transitionend',
+		() => {
+			albumPages[currentPage].style.zIndex = '0';
+			currentPage++;
+		},
+		{ once: true }
 	);
-	console.log(response);
 };
-fetchData();
 
-let albumState: 0 | 1 = 0;
-const flipAlbum = () => {
-
+const prevPage = () => {
+	if (currentPage <= 0) return;
+	currentPage--;
+	albumPages[currentPage].classList.remove('flipped');
+	albumPages[currentPage].style.zIndex = String(albumPages.length - currentPage);
 };
+
+nextButton.addEventListener('click', nextPage);
+prevButton.addEventListener('click', prevPage);
+
+initAlbum();

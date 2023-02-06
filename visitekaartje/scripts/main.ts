063fa8fafec5269754d109316e2a7968e09543de
Laurens256 <duinlaurens2@gmail.com>
2023-02-06 20:33:17 +0100

    update page flipping

--- visitekaartje/scripts/main.ts
+++ visitekaartje/scripts/main.ts
@@ -1,4 +1,8 @@
+const container = document.querySelector('.container')!;
 const albumPages: NodeListOf<HTMLElement> = document.querySelectorAll('.album > section');
+const pageControls = document.querySelectorAll('.controls button')!;
+const nextButton = document.querySelector('.next-page-btn')!;
+const prevButton = document.querySelector('.prev-page-btn')!;
 
 const initAlbum = () => {
 	for (let i = 0; i < albumPages.length; i++) {
@@ -6,33 +10,21 @@ const initAlbum = () => {
 	}
 };
 
-const nextButton = document.querySelector('.next-page-btn')!;
-const prevButton = document.querySelector('.prev-page-btn')!;
-
-let currentPage = 0;
-const nextPage = () => {
-	if (currentPage >= albumPages.length - 1) return;
-
-	albumPages[currentPage].classList.add('flipped');
-	console.log(albumPages[currentPage]);
-	albumPages[currentPage].addEventListener(
-		'transitionend',
-		() => {
-			albumPages[currentPage].style.zIndex = '0';
-			currentPage++;
-		},
-		{ once: true }
-	);
+let pageState: 0 | 1 = 0;
+const changePage = (e: Event) => {
+	const target = e.currentTarget as HTMLElement;
+	if (target.classList.contains('next-page-btn') && pageState === 0) {
+		pageState = 1;
+		albumPages[pageState - 1].classList.add('flipped');
+	} else if (target.classList.contains('prev-page-btn') && pageState === 1) {
+		pageState = 0;
+		albumPages[pageState].classList.remove('flipped');
+	} else {
+		return;
+	}
+	container.classList.toggle('flipped');
 };
 
-const prevPage = () => {
-	if (currentPage <= 0) return;
-	currentPage--;
-	albumPages[currentPage].classList.remove('flipped');
-	albumPages[currentPage].style.zIndex = String(albumPages.length - currentPage);
-};
-
-nextButton.addEventListener('click', nextPage);
-prevButton.addEventListener('click', prevPage);
+pageControls.forEach((button) => button.addEventListener('click', changePage));
 
 initAlbum();

98923bc02186063db0c4751a416e6084b1b66ab3
Laurens256 <duinlaurens2@gmail.com>
2023-02-07 13:07:16 +0100

    add fallback data (broken)

---
+++ visitekaartje/assets/defaultData.ts
@@ -0,0 +1,16 @@
+export const defaultData = {
+	member: {
+		avatar: 'https://avatars.githubusercontent.com/u/85798751?v=4',
+		bio: { html: '' },
+		gitHubHandle: 'Laurens257',
+		id: 'cldep8zqq3wbh0av00ktcml8w',
+		name: 'Laurens',
+		nickname: 'Laurens256',
+		prefix: 'dr',
+		role: ['student'],
+		slug: 'garfield-enjoyer',
+		squads: [{ cohort: '2023', name: 'Minor Web', slug: 'minor-web-2023', website: null }],
+		surname: 'Duin',
+		website: ''
+	}
+};
--- visitekaartje/scripts/main.ts
+++ visitekaartje/scripts/main.ts
@@ -1,4 +1,5 @@
-import { User } from '../assets/types';
+import { User } from 'assets/types';
+// import { defaultData } from 'assets/defaultData';
 
 const container = document.querySelector('.container')!;
 const pageControls = document.querySelectorAll('.controls button')!;
@@ -10,10 +11,14 @@ const defaultSquadId = 'cldcspecf0z0o0bw59l8bwqim';
 const fetchUser = async () => {
 	container.classList.add('loading');
 	const id = new URLSearchParams(window.location.search).get('id') || defaultUserId;
-	const data: User = await (
-		await fetch(`https://whois.fdnd.nl/api/v1/member?id=${id}`)
-	).json();
-	generateUser(data.member);
+	let user!: User;
+	try {
+		user = await (await fetch(`https://whois.fdnd.nl/api/v1/member?id=${id}`)).json();
+	} catch {
+		// user = defaultData;
+		console.log('Fout bij het ophalen van data, standaard gebruiker wordt geladen');
+	}
+	generateUser(user.member);
 	container.classList.remove('loading');
 };
 
@@ -27,13 +32,13 @@ const albumTitle: HTMLHeadingElement = document.querySelector(
 // laadt data in visitekaartje
 const generateUser = (user: User['member']) => {
 	albumCover.addEventListener('load', () => {
-		if(user.name) {
+		if (user.name) {
 			albumCover.alt = `avatar van ${user.name}`;
 			albumTitle.textContent = user.name;
 		} else {
-			albumCover.alt = 'albumhoes'
+			albumCover.alt = 'albumhoes';
 		}
-		if(user.nickname) {
+		if (user.nickname) {
 			albumTitle.textContent = user.nickname;
 		}
 	});

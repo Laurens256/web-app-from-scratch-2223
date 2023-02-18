b19f88a345a66a72b587946cc2f43e2ea56ba593
Laurens256 <duinlaurens2@gmail.com>
2023-02-19 00:52:25 +0100

    fix template in prod

--- pokedex/scripts/template/loadTemplate.ts
+++ pokedex/scripts/template/loadTemplate.ts
@@ -1,5 +1,13 @@
+import { pokemonListItem } from '../../templates/pokemonListItem';
+
+// const loadTemplate = async (templateName: string) => {
+// 	const template = await (await fetch(`/templates/${templateName}.tmpl`)).text();
+// 	return template;
+// };
+
 const loadTemplate = async (templateName: string) => {
-	const template = await (await fetch(`/templates/${templateName}.tmpl`)).text();
+	// heel crappy maar fix later wel als ik zin heb
+	const template = templateName === 'pokemon-list-item' ? pokemonListItem : '';
 	return template;
 };
 
---
+++ pokedex/templates/pokemonListItem.ts
@@ -0,0 +1,2 @@
+export const pokemonListItem =
+	'<li class="loading skeleton"><button><section><p></p></section><section><h2></h2></section><section><div></div></section></button></li>';

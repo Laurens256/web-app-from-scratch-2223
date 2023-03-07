04aa48e197d98a1eb8d91318d1a612c89f4f298a
Laurens256 <duinlaurens2@gmail.com>
2023-03-07 21:24:29 +0100

    made header cooler

--- scripts/views/headerView.ts
+++ scripts/views/headerView.ts
@@ -1,23 +1,19 @@
 import { viewNames } from '../routing/routes';
 
 const headerElement = document.querySelector<HTMLElement>('header');
-const h1Element = headerElement?.querySelector<HTMLHeadingElement>('h1');
-const h2Element = headerElement?.querySelector<HTMLHeadingElement>('h2');
-
+const headerUl = headerElement?.querySelector<HTMLUListElement>('header ul');
 
 type headerType = {
-	[K in viewNames]?: { h1: string; h2: string; };
+	[K in viewNames]?: { h1: string; h2?: string };
 };
 
 
 const headers: headerType = {
 	listview: {
 		h1: 'POKéMON&nbsp;&nbsp;LIST',
-		h2: ''
 	},
 	detailview: {
 		h1: 'POKéMON&nbsp;&nbsp;LIST',
-		h2: ''
 	},
 	filterview: {
 		h1: 'POKéDEX',
@@ -29,14 +25,44 @@ const headers: headerType = {
 	}
 };
 
+// const HeaderView = (view: viewNames) => {
+// 	if (!headerElement || !headerH1Li || !headerH2Li) return;
+// 	headerH1Li.innerHTML = '';
+// 	headerH2Li.innerHTML = '';
+// 	if (!headers[view]) {
+// 		headerElement?.classList.add('hidden');
+// 	} else {
+// 		headerElement?.classList.remove('hidden');
+
+// 		const h1Element = document.createElement('h1');
+// 		h1Element.innerHTML = headers[view]!.h1;
+// 		headerH1Li!.appendChild(h1Element);
+
+// 		if (headers[view]!.h2 !== '') {
+// 			console.log(headers[view]!.h2);
+// 			const h2Element = document.createElement('h2');
+// 			h2Element.innerHTML = headers[view]!.h2;
+// 			headerH2Li!.appendChild(h2Element);
+// 		}
+// 	}
+// };
+
 const HeaderView = (view: viewNames) => {
-	if(!headerElement) return;
+	if (!headerElement || !headerUl) return;
+	headerUl.innerHTML = '';
 	if (!headers[view]) {
 		headerElement?.classList.add('hidden');
-	} else if (h1Element && h2Element) {
+	} else {
 		headerElement?.classList.remove('hidden');
-		h1Element.innerHTML = headers[view]!.h1;
-		h2Element.innerHTML = headers[view]!.h2;
+
+		for (const key in headers[view]) {
+			const li = document.createElement('li');
+			const element = document.createElement(key);
+			const value: string = headers[view]![key as keyof typeof headers[typeof view]];
+			element.innerHTML = value;
+			li.appendChild(element);
+			headerUl.appendChild(li);
+		}
 	}
 };
 
